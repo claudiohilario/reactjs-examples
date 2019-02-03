b5308f39305200b55c4a3478e1a3aa152076d0bc
Cláudio Hilário <claudio.hilario@ontech.pt>
2019-02-03 14:58:12 +0000

    [REFACTOR] Refactor counter

--- src/containers/couterExample/index.js
+++ src/containers/couterExample/index.js
@@ -1,28 +1,49 @@
 import React from "react";
 
-class CounterExample extends React.Component {
+class Counter extends React.Component {
   state = {
     count: 0
   };
 
-  handleClick = () => this.setState(state => ({ count: state.count + 1 }));
-  handleReset = () => this.setState({ count: 0 });
+  updateCount = val => this.setState({ count: val });
 
   render() {
-    const { dark } = this.props;
-    const theme = dark ? "dark" : "";
-    return (
-      <div className={`clickerLayout ${theme}`}>
-        <span className="clickerCount" onClick={this.handleClick}>
-          {this.state.count}
-        </span>
-        <button className="primary" onClick={this.handleReset}>
-          Reset
-        </button>
-      </div>
-    );
+    const stateTools = [this.state.count, this.updateCount];
+    return this.props.children(stateTools);
   }
 }
+
+const Clicker = props => {
+  const { count, dark, onClick, onReset } = props;
+  const theme = dark ? "dark" : "";
+
+  return (
+    <div className={`clickerLayout ${theme}`}>
+      <span className="clickerCount" onClick={onClick}>
+        {count}
+      </span>
+      <button className="primary" onClick={onReset}>
+        Reset
+      </button>
+    </div>
+  );
+};
+
+const CounterExample = () => {
+  return (
+    <Counter>
+      {([count, updateCount]) => {
+        return (
+          <Clicker
+            count={count}
+            onClick={() => updateCount(count + 1)}
+            onReset={() => updateCount(0)}
+          />
+        );
+      }}
+    </Counter>
+  );
+};
 
 CounterExample.defaultProps = {
   dark: true
